ae868c12657e8868efc0d9fed99d859bd3a6b78e
Shakib448 <shakiba448@gmail.com>
2020-08-06 09:42:04 +0600

    first commit

---
+++ letConst.js
@@ -0,0 +1,12 @@
+const hubby = "Hero alom";
+console.log(hubby);
+
+const numbers = [12,45];
+numbers[1] = 88;
+
+numbers.push(22);
+// console.log(numbers);
+
+let patientName = "Rahim chaca"
+patientName = 'shakib khan';
+console.log(patientName);
