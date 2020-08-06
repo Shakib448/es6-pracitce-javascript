66beecdd75682ad0eaec2fa28022788cffb47821
Shakib448 <shakiba448@gmail.com>
2020-08-06 13:19:43 +0600

    first commit

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Justin";
+const lastName = "TImberLake";
+
+const fullName = firstName + ' ' + lastName + ' is a good by';
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+
+const multiLine = `I love you
+I miss you
+I don't need you`
+
+console.log(multiLine);
