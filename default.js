e35d44c4302536d0fccb4a0723d29cdc735fa97c
Shakib448 <shakiba448@gmail.com>
2020-08-06 09:52:35 +0600

    first commit

---
+++ default.js
@@ -0,0 +1,6 @@
+function add (num1 , num2 = 0){
+    return num1 + num2;
+}
+
+const total = add(15);
+console.log(total);
--- letConst.js
+++ letConst.js
@@ -7,6 +7,10 @@ numbers[1] = 88;
 numbers.push(22);
 // console.log(numbers);
 
+for (let i = 0; i < 10; i++) {
+    sum = sum + 1;    
+}
+
 let patientName = "Rahim chaca"
 patientName = 'shakib khan';
 console.log(patientName);
